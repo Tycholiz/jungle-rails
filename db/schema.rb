d415505a6ff2957ac53a3457c4a75b346fa98414
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-09-14 15:19:27 -0700

    set up db structure to include reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180914003343) do
+ActiveRecord::Schema.define(version: 20180914220003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,13 +56,18 @@ ActiveRecord::Schema.define(version: 20180914003343) do
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "user_id"
+    t.text    "description"
+    t.integer "rating"
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
   end
 
   add_foreign_key "line_items", "orders"
