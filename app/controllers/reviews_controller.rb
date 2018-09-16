a7eca6fae62da937f3fcc0604b33b1b4267400d0
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-09-16 23:35:42 +0000

    add reverse order to reviews and dont give ability for non users to review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,15 @@
 class ReviewsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
-    @review = @product.reviews.create
+    @review = @product.reviews.create(review_params)
     @review.user = current_user
     if @review.save
       redirect_to :back
     end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:description, :rating).merge(user: current_user)
   end
 end
