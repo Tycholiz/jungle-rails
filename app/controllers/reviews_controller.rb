d8b402cc9bccff7f1823a78c352b46bf2aa73d06
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-09-16 03:30:32 +0000

    review box will now redirect to same page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,10 @@
 class ReviewsController < ApplicationController
   def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.create
+    @review.user = current_user
+    if @review.save
+      redirect_to :back
+    end
   end
 end
