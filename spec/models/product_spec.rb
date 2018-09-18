86219bad56134f80e0d87074fba19fbbea171bde
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-09-18 21:19:22 +0000

    complete tests for product and attributes

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe "Valdiations" do
+    before(:each) do
+      @category = Category.create(name: "Apparel")
+    end
+
+    it "should save with valid attributes" do
+      product = Product.new(
+        name: "Nice shirt",
+        price: 24.95,
+        quantity: 55,
+        category_id: @category.id
+      )
+      expect(product).to be_valid
+    end
+
+    it "is not valid without a name" do
+      product = Product.new(
+        name: nil,
+        price: 24.95,
+        quantity: 55,
+        category_id: @category.id
+      )
+      expect(product).to_not be_valid
+    end
+
+    it "is not valid without a price" do
+      product = Product.new(
+        name: "Nice shirt",
+        price: nil,
+        quantity: 55,
+        category_id: @category.id
+      )
+      expect(product).to_not be_valid
+    end
+
+    it "is not valid without a quantity" do
+      product = Product.new(
+        name: "Nice shirt",
+        price: 24.95,
+        quantity: nil,
+        category_id: @category.id
+      )
+      expect(product).to_not be_valid
+    end
+
+    it "is not valid without a category" do
+      product = Product.new(
+        name: "Nice shirt",
+        price: 24.95,
+        quantity: 55,
+        category_id: nil
+      )
+      expect(product).to_not be_valid
+    end
+  end
+end
